5c70144a13388aa4a973bee9f645d306104bf1ce
Weslley Lucas <weslusisa@gmail.com>
2025-04-28 22:04:50 -0300

    add some tests

--- test/models/contact_list_test.rb
+++ test/models/contact_list_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
 
 class ContactListTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be valid with name and phone" do
+    contact = ContactList.new(name: "John Doe", phone: "123456789")
+    assert contact.valid?
+  end
+
+  test "should not be valid without name" do
+    contact = ContactList.new(phone: "123456789")
+    assert_not contact.valid?
+    assert_includes contact.errors[:name], "can't be blank"
+  end
+
+  test "should not be valid without phone" do
+    contact = ContactList.new(name: "John Doe")
+    assert_not contact.valid?
+    assert_includes contact.errors[:phone], "can't be blank"
+  end
+
+  test "name should not be blank" do
+    contact = ContactList.new(name: "", phone: "123456789")
+    assert_not contact.valid?
+    assert_includes contact.errors[:name], "can't be blank"
+  end
+
+  test "phone should not be blank" do
+    contact = ContactList.new(name: "John Doe", phone: "")
+    assert_not contact.valid?
+    assert_includes contact.errors[:phone], "can't be blank"
+  end
 end
